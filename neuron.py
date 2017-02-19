1fb48239c4ed7b36dbd4e272a5e71e6daa3273f3
pxb2841 <pxb2841@mail.rit.edu>
2017-02-18 20:22:08 -0500

    added method signatures and class

--- neuron.py
+++ neuron.py
@@ -4,3 +4,20 @@ File contains logic to properly train a Neuron.
 Author: Philip Bedward
 Author: Rachael Thormann
 """
+
+class ParentNeuron:
+
+    def __init__(self):
+        pass
+
+    def __str__(self):
+        pass
+
+    def activate(self):
+        pass
+
+    def feedForward(self):
+        pass
+
+    def train(self):
+        pass
